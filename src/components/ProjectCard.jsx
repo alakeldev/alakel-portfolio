4315f94d867abaecf8a3b09149ec97f1751976f6
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-11-06 15:47:08 +0100

    edit button on projects cards view live project and view repo

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,17 +6,17 @@ const ProjectCard = (props) => {
     <div className='col-12 col-md-6 col-lg-4'>
       <Card className='shadow card'>
         <Card.Img variant='top' src={props.projectImage} className='cardImage' />
-        <Button className='shadow text-capitalize view-project-btn live-btn'onClick={() => window.open(props.liveProjectLink, '_blank')}>View Live Project</Button>
         <Card.Body className='p-4 card-body'>
           <Card.Title className='fw-bold text-uppercase mb-3 fs-4'>
             {props.projectName}
           </Card.Title>
           <Card.Text>{props.projectDescription}</Card.Text>
-          <Button className='shadow text-capitalize view-project-btn' onClick={(e) => {
+        </Card.Body>
+        <Button className='shadow text-capitalize view-project-btn live-btn'onClick={() => window.open(props.liveProjectLink, '_blank')}>View Live Project</Button>
+        <Button className='shadow text-capitalize view-project-btn' onClick={(e) => {
             e.stopPropagation();
             window.open(props.repoLink, '_blank');
           }}>View Repository</Button>
-        </Card.Body>
       </Card>
     </div>
   );
