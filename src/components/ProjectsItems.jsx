e63714653fcb46e1ab6cd25ad4689f2bdf7a6e5c
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-12-29 15:48:53 +0100

    edit ProjectsItems.jsx 'added new project + edit some links for the old projects'

--- src/components/ProjectsItems.jsx
+++ src/components/ProjectsItems.jsx
@@ -3,7 +3,7 @@ import Img2 from '../assets/images/pimg2.png';
 import Img3 from '../assets/images/pimg3.png';
 import Img4 from '../assets/images/pimg4.png';
 import Img5 from '../assets/images/pimg5.png';
-
+import Img6 from '../assets/images/pimg6.png';
 
 const ProjectsItems = [
     {
@@ -11,16 +11,16 @@ const ProjectsItems = [
         projectImg: Img1,
         projectTitle: 'Call Of Duty',
         projectDescription: 'Ghost Team website offers comprehensive information on the latest events, championships, updates, and photo galleries related to the Call of Duty game. It is developed using fundamental HTML5 and CSS3 technologies.',
-        liveProjectLink: 'https://alakeldev.github.io/ghost-cod-team-website-pp1/',
-        repoLink: 'https://github.com/alakeldev/ghost-cod-team-website-pp1'
+        liveProjectLink: 'https://alakeldev.github.io/ghost-cod-team-website/',
+        repoLink: 'https://github.com/alakeldev/ghost-cod-team-website'
     },
     {
         id: 2,
         projectImg: Img2,
         projectTitle: 'To-Do List',
         projectDescription: 'This application assists users in managing their tasks and tracking daily progress on various projects. It is developed using Vanilla JavaScript.',
-        liveProjectLink: 'https://alakeldev.github.io/to-do-list-pp2/',
-        repoLink: 'https://github.com/alakeldev/to-do-list-pp2'
+        liveProjectLink: 'https://alakeldev.github.io/to-do-list-app/',
+        repoLink: 'https://github.com/alakeldev/to-do-list-app'
     },
     {
         id: 3,
@@ -28,7 +28,7 @@ const ProjectsItems = [
         projectTitle: 'Passwords Manager',
         projectDescription: 'This application generates new passwords and manages existing ones, securely storing them in a single file. It employs a symmetric key for encrypting and decrypting your data and is developed using the Python programming language.',
         liveProjectLink: 'https://password-generator-and-manager-a88bcb86c5e0.herokuapp.com/',
-        repoLink: 'https://github.com/alakeldev/passwords-generator-and-manager-pp3'
+        repoLink: 'https://github.com/alakeldev/passwords-generator-and-manager-app'
     },
     {
         id: 4,
@@ -36,7 +36,7 @@ const ProjectsItems = [
         projectTitle: 'Home Taste',
         projectDescription: 'Chefs Community, Whether you are a seasoned cook or a curious food enthusiast, our website provides a delightful experience for home chefs. It is developed using the Python programming language (the Django full-stack framework, and the Jinja templating language).',
         liveProjectLink: 'https://home-taste-2aece88c850a.herokuapp.com/',
-        repoLink: 'https://github.com/alakeldev/home-taste-pp4'
+        repoLink: 'https://github.com/alakeldev/home-taste-app'
     },
     {
         id: 5,
@@ -44,7 +44,15 @@ const ProjectsItems = [
         projectTitle: 'Techplaza Platform',
         projectDescription: 'This platform offers extensive solutions for application development and IT support. It is developed using JavaScript (React.js) for the frontend and Python (Django Rest Framework) for the backend API.',
         liveProjectLink: 'https://frontend-techplaza-d0af91d53972.herokuapp.com/',
-        repoLink: 'https://github.com/alakeldev/frontend-techplaza-pp5'
+        repoLink: 'https://github.com/alakeldev/techplaza-app-frontend'
+    },
+    {
+        id: 6,
+        projectImg: Img6,
+        projectTitle: 'Hi Translate Me',
+        projectDescription: 'Built with Next.js and TypeScript, Hi Translate Me is a voice translation tool that translates spoken text into another language and plays back the translation. This project integrates speech recognition and translation APIs.',
+        liveProjectLink: 'https://hi-translate-me-0754d2a11817.herokuapp.com/',
+        repoLink: 'https://github.com/alakeldev/hi-translate-me-app'
     },
 ]
 
