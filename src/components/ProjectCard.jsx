2eb6826d06bc678da83cb62ae7619d5d64dc30f0
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2025-01-03 10:42:25 +0100

    edit ProjectCard.jsx use destructuring, remove pointer cursor from the main project card edit custom.scss

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const ProjectCard = (props) => {
+const ProjectCard = (project) => {
+  const {projectName, projectDescription, projectImage, liveProjectLink, repoLink} = project;
   return (
     <div className='col-12 col-md-6 col-lg-4'>
       <Card className='shadow card'>
-        <Card.Img variant='top' src={props.projectImage} className='cardImage' />
+        <Card.Img variant='top' src={projectImage} className='cardImage' />
         <Card.Body className='p-4 card-body'>
           <Card.Title className='fw-bold text-uppercase mb-3 fs-4'>
-            {props.projectName}
+            {projectName}
           </Card.Title>
-          <Card.Text>{props.projectDescription}</Card.Text>
+          <Card.Text>{projectDescription}</Card.Text>
         </Card.Body>
-        <Button className='shadow text-capitalize view-project-btn live-btn'onClick={() => window.open(props.liveProjectLink, '_blank')}>View Live Project</Button>
-        <Button className='shadow text-capitalize view-project-btn' onClick={(e) => {
-            e.stopPropagation();
-            window.open(props.repoLink, '_blank');
-          }}>View Repository</Button>
+        <Button className='shadow text-capitalize view-project-btn live-btn'onClick={() => window.open(liveProjectLink, '_blank')}>View Live Project</Button>
+        <Button className='shadow text-capitalize view-project-btn' onClick={() => window.open(repoLink, '_blank')}>View Repository</Button>
       </Card>
     </div>
   );
